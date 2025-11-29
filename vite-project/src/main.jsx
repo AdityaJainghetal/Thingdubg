88b0d24cca518932e0cbb2e07fd19b44f31b7eb7
Aditya Jain <adityajainghetal@gmail.com>
2025-11-29 16:55:11 +0530

    Task

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,14 +1,11 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import App from "./App.jsx";
 import { CartProvider } from "./context/CartContext";
 ReactDOM.createRoot(document.getElementById("root")).render(
-   <CartProvider>
-  
+  <CartProvider>
     <App />
-
   </CartProvider>
 );
